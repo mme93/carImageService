d8aac9c87033a376a47a87fee90f8d538ee0890f
magantus@googlemail.com <Extrem93!>
2021-07-15 22:09:02 +0200

    first commit

---
+++ src/main/java/de/carImgService/CarImgServiceApplication.java
@@ -0,0 +1,13 @@
+package de.carImgService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarImgServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarImgServiceApplication.class, args);
+	}
+
+}
